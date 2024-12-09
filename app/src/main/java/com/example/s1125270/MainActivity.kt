06f85370f96a3155d3645d5a823f51fd83d1b9b3
黃歆芝的實力 <bella12345694@gmail.com>
2024-12-09 16:49:59 +0800

    v2_黃歆芝

--- app/src/main/java/com/example/s1125270/MainActivity.kt
+++ app/src/main/java/com/example/s1125270/MainActivity.kt
@@ -1,17 +1,51 @@
 package com.example.s1125270
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import com.example.s1125270.ui.theme.S1125270Theme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.platform.LocalContext
+import android.app.Activity
+import androidx.compose.ui.Alignment
+
+import androidx.compose.material3.Button
+import androidx.compose.foundation.Image
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.material3.Button
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,29 +54,99 @@ class MainActivity : ComponentActivity() {
         setContent {
             S1125270Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    Birth(m = Modifier.padding(innerPadding),)
+                      /*Greeting(name = "Android",  Modifier.background(Color(0xff95fe95)))*/
+
+
                 }
             }
         }
     }
+
+
 }
 
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+
+
+fun Birth(m: Modifier) {
+    var appear by remember { mutableStateOf(true) }
+    val backgroundColor by animateColorAsState(
+        if (appear) {
+            Color.Transparent
+        } else Color.Green,
+        animationSpec = tween(2000, 500)
     )
+
+    LazyColumn(Modifier.background(backgroundColor)) {
+       item() {
+           Modifier.background(Color(0xff95fe95))
+       }
+
+           item(){
+              modifierLocalOf {  }
+               Text(text = "2024期末上機考(資管二A黃歆芝)")
+
+           }
+           item(){
+
+              Image(painter = painterResource(id=R.drawable.class_a),contentDescription = "Class_a")
+
+              }
+        item(){
+            Text(text = "遊戲持續時間 0 秒")
+        }
+        item(){
+            Text(text = "您的成績 0 分")
+        }
+           item(){
+               Button(onClick = {})
+
+                {
+                   Text(text ="結束App" )
+               }
+           }
+
+            }
+
+
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S1125270Theme {
-        Greeting("Android")
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
